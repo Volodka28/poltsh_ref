93f6a271e9b4b87be12b5f119b43d26527b2b8fb
Volodka28 <buley28@gmail.com>
2022-01-20 15:36:03 +0300

    qq
    0.1.2

--- Lazurit/__main__.py
+++ Lazurit/__main__.py
@@ -2,11 +2,13 @@ from pathlib import Path
 import os
 from sys import platform
 import argparse
+import mkCase.init as mkCase
 
 import yaml
 from yaml.loader import SafeLoader
 import toml
 import json
+
 
 def read_autotest_config(path, name_config="start_conf"):
     autotest_conf_path = None
@@ -28,6 +30,22 @@ def read_autotest_config(path, name_config="start_conf"):
     return autotest_conf
 
 
+def init_case(case_name, test_path, program, program_version):
+    for task_name in autotest_config["cases"][case_name]["tasks"]:
+        new_task = mkCase.Task(case_name=case_name,
+                               task_name=task_name,
+                               test_path=test_path,
+                               program=program,
+                               program_version=program_version)
+        new_task.make_dirs()
+        new_task.make_stat()
+        tasks_to_run[case_name].append(new_task)
+        print("Создано:", new_task.case_name, new_task.task_name, sep="---")
+
+
+def start_task():
+    pass
+
 
 if __name__ == "__main__":
     # считываем конфиг
@@ -39,13 +57,35 @@ if __name__ == "__main__":
     args = parser.parse_args()
     autotest_config["test_path"] = args.test_path
     autotest_config["case_path"] = args.case_path
-    # выяснение текущей операционной системы для определения сценария
-    cur_os = platform
+
+    # # выяснение текущей операционной системы для определения сценария
+    # cur_os = platform
+
     #### ИНИЦИАЛИЗАЦИЯ КЕЙСОВ
+
+    events = [mkCase.Event(mkCase.MESH_CREATE), mkCase.Event(mkCase.CONFIG_CREATE)]
+    event_giver = mkCase.EventGiver()
+    for event in events:
+        event_giver.add_event(event)
+
+    tasks_to_run = {}
     for case in autotest_config["cases"]:
-        pass
+        if (Path(autotest_config["case_path"]) / case).exists():
+            tasks_to_run[case] = []
+            init_case(case_name=case,
+                      test_path=Path(autotest_config["test_path"]),
+                      program=autotest_config["program"],
+                      program_version=autotest_config["program_version"])
+        else:
+            raise KeyError(f"Такого кейса нет в базе: {case}")
 
-
+    for case_name in tasks_to_run:
+        for task in tasks_to_run[case_name]:
+            event_giver.handle_events(task)
+            print(task.taken_events)
+            print(task.passed_events)
+            event_giver.handle_events(task)
+            print(task.taken_events)
+            print(task.passed_events)
 
     #### ЗАПУСК НА РАСЧЁТ
-
--- Lazurit/mkCase/init.py
+++ Lazurit/mkCase/init.py
@@ -1,10 +1,8 @@
 import os
 from pathlib import Path
 import shutil
-
-
-
-
+import json
+import yaml
 
 MESH_CREATE, CONFIG_CREATE = "MESH_CREATE", "CONFIG_CREATE"
 
@@ -12,6 +10,9 @@ MESH_CREATE, CONFIG_CREATE = "MESH_CREATE", "CONFIG_CREATE"
 class Task:
 
     def __init__(self, case_name: str, task_name: str, test_path: Path, program: str, program_version: str):
+        self.post_path = None
+        self.done_path = None
+        self.calc_path = None
         self.case_name = case_name
         self.task_name = task_name
         self.test_path = test_path
@@ -20,19 +21,26 @@ class Task:
         self.passed_events = set()
         self.taken_events = set()
 
+    def make_stat(self):
+        params = dict(self.__dict__)
+        for param in params:
+            params[param] = str(params[param])
+        with open(self.calc_path / "stat.yml", "w") as f:
+            yaml.dump(params, f)
 
     def make_dirs(self):
         path_name = f"{self.case_name}_{self.task_name}"
-        calc_path = self.test_path / "calc" / self.program / self.program_version / path_name
-        done_path = self.test_path / "done" / self.program / self.program_version / path_name
-        post_path = self.test_path / "post" / self.program / self.program_version / path_name
-        if not done_path.exists():
-            if calc_path.exists():
-                shutil.rmtree(calc_path, ignore_errors=True)
-            os.makedirs(calc_path, exist_ok=True)
-        if post_path.exists():
-            shutil.rmtree(post_path, ignore_errors=True)
-
+        self.calc_path = self.test_path / "calc" / self.program / self.program_version / path_name
+        self.done_path = self.test_path / "done" / self.program / self.program_version / path_name
+        self.post_path = self.test_path / "post" / self.program / self.program_version / path_name
+        if not self.done_path.exists():
+            if self.calc_path.exists():
+                shutil.rmtree(self.calc_path, ignore_errors=True)
+            os.makedirs(self.calc_path, exist_ok=True)
+        else:
+            print(f"Задача {self.case_name}_{self.task_name} уже посчитана")
+        if self.post_path.exists():
+            pass
 
 
 class Event:
@@ -60,6 +68,7 @@ class MeshCreate(NullHandler):
                 print(f"Задание получено: \"{event_name}\"")
                 task.taken_events.add(event.kind)
             elif event.kind in task.taken_events:
+                os.makedirs(task.calc_path / "MESH_CREATE", exist_ok=True)
                 print(f"Задание выполнено: \"{event_name}\"")
                 task.passed_events.add(event.kind)
                 task.taken_events.remove(event.kind)
@@ -77,6 +86,7 @@ class ConfigCreate(NullHandler):
                 print(f"Задание получено: \"{event_name}\"")
                 task.taken_events.add(event.kind)
             elif event.kind in task.taken_events:
+                os.makedirs(task.calc_path / "CONFIG_CREATE", exist_ok=True)
                 print(f"Задание выполнено: \"{event_name}\"")
                 task.passed_events.add(event.kind)
                 task.taken_events.remove(event.kind)
@@ -107,21 +117,17 @@ if __name__ == "__main__":
     for event in events:
         event_giver.add_event(event)
 
-
-    task = Task(case_name="case",
+    task = Task(case_name="ROG_15",
                 task_name="task",
                 test_path=Path(r"C:\Users\vvbuley\Desktop\polish_test"),
                 program="Lazurit",
                 program_version="Lazurit_0.1")
+
     task.make_dirs()
-
-    event_giver.handle_events(task)
-    print()
-    task.taken_events = {MESH_CREATE}
-    event_giver.handle_events(task)
-    print()
-    event_giver.handle_events(task)
-
-
-
-
+    task.make_stat()
+    # event_giver.handle_events(task)
+    # print(task.taken_events)
+    # print(task.passed_events)
+    # event_giver.handle_events(task)
+    # print(task.taken_events)
+    # print(task.passed_events)
