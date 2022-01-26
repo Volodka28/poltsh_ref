ca5c42b0b12af41fd009383365de0206f838a0e3
Volodka28 <buley28@gmail.com>
2022-01-26 15:17:53 +0300

    Refactoring

--- run_util/__main__.py
+++ run_util/__main__.py
@@ -7,17 +7,5 @@ import json
 import toml
 from yaml.loader import SafeLoader
 
-class AbstractRun():
-    def __init__(self):
-
-    def temolate_method(self):
-        self.read_template()
-        self.fill_template()
-        self.run_template()
-
-    def read_template(self):
-
-
-
 if __name__ == "__main__":
     pass
