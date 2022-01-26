5cbcdd7fb253f59193c142606677ebe8896ec607
Volodka28 <buley28@gmail.com>
2022-01-26 17:19:44 +0300

    read_template_complet

--- run_util/__main__.py
+++ run_util/__main__.py
@@ -32,59 +32,63 @@ def read_file(path, name_file="start_conf"):
 
 class AbstractTaskRun(ABC):
     def __init__(self):
-        self.fill_temp = None
+        self.template = None
 
-    def template_method(self, template):
+    def template_method(self, template, task_path, autotest_config):
         self.read_template(template)
-        self.fill_template()
-        self.make_passport()
+        self.fill_template(task_path, autotest_config)
+        self.make_passport(task_path, autotest_config)
         self.run_task()
 
     def read_template(self, template):
         with open(template, "r") as f:
-            self.fill_temp = f.read()
+            self.template = f.read()
 
     @abstractmethod
-    def fill_template(self) -> None:
+    def fill_template(self, task_path, autotest_config) -> None:
         pass
 
     @abstractmethod
     def run_task(self) -> None:
         pass
 
-    def make_passport(self) -> None:
+    def make_passport(self, task_path, autotest_config) -> None:
         pass
 
 
 class WindowsRunTask(AbstractTaskRun):
-    def fill_template(self) -> None:
-        print("win_fill")
+    def fill_template(self, task_path, autotest_config) -> None:
+        with open(task_path / "start.bat", "w") as f:
+            f.write(self.template)
 
     def run_task(self) -> None:
         print("win_run")
 
 
 class LinuxRunTask(AbstractTaskRun):
-    def fill_template(self) -> None:
-        print("fill_linux")
+    def fill_template(self, task_path, autotest_config) -> None:
+        with open(task_path / "start.sh", "w") as f:
+            f.write(self.template)
 
     def run_task(self) -> None:
         print("run_linux")
 
-    def make_passport(self) -> None:
+    def make_passport(self, task_path, autotest_config) -> None:
         print("passport_linux")
 
 
-def run_task(abstract_class: AbstractTaskRun, template):
-    abstract_class.template_method(template)
+def run_task(abstract_class: AbstractTaskRun, template, task_path, autotest_config):
+    abstract_class.template_method(template, task_path, autotest_config)
 
 
 if __name__ == "__main__":
     template = Path(r'D:\project\GIT\poltsh_ref\Lazurit\runTask\templates\run.bat')
+    task_path = Path(r"C:\Users\vvbuley\Desktop\polish_test\calc\lazurit\Lazurit.WIN.AVX.exe\ROG_15_y_0_1")
+    autotest_config = read_file(path=Path(r"C:\Users\vvbuley\Desktop\polish_test"), name_file="start_conf")
     match platform:
         case "win32":
-            run_task(WindowsRunTask(), template)
+            run_task(WindowsRunTask(), template, task_path, autotest_config)
         case "linux":
-            run_task(LinuxRunTask(), template)
+            run_task(LinuxRunTask(), template, task_path, autotest_config)
         case _:
             raise ValueError("Данная система не поддерживает запуск приложения")
