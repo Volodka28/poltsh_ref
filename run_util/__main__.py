22cf00584466f8c22347164220d53bcf0f7949ca
Volodka28 <buley28@gmail.com>
2022-02-18 17:23:19 +0300

    update start on cluster

--- run_util/__main__.py
+++ run_util/__main__.py
@@ -108,32 +108,56 @@ class WindowsRunTask(AbstractTaskRun):
 
 class LinuxRunTask(AbstractTaskRun):
     def fill_template(self, task_path, autotest_config) -> None:
+        threads_count = {
+            "knl": 8,
+            "broadwell": 8,
+            "aviator3": 8,
+            "haswell": 8,
+            "skylake": 8,
+            "cascadelake": 8
+        }
+        jcpu_node = {
+            "knl": 8,
+            "broadwell": 8,
+            "aviator3": 8,
+            "haswell": 8,
+            "skylake": 8,
+            "cascadelake": 8
+        }
+        
+        task_setting = read_file(task_path, "stat.")
+        init_task = autotest_config["cases"][task_setting["case_name"]]["tasks"][task_setting["task_name"]]
+        match [init_task['dont'], init_task['init']]:
+            case ["dont", "mesh"]:
+                comand = f"{autotest_config['program_version']} --dont --import_mesh --threads {threads_count[autotest_config['node_type']]}"
+            case ["dont", "data"]:
+                comand = f"{autotest_config['program_version']} --dont --threads {threads_count[autotest_config['node_type']]}"
+            case ["cont", "data"]:
+                comand = f"{autotest_config['program_version']} --cont -1 --threads {threads_count[autotest_config['node_type']]}"
+            case _:
+                raise KeyError('недопустимые значения для инициализации задачи')
+        fill_template = self.template.format(start_str=comand,
+                                             nodes_count=autotest_config["node_count"],
+                                             jcpu_node=jcpu_node[autotest_config['node_type']],
+                                             type_node=autotest_config['node_type'])
         with open(task_path / "start.sh", "w") as f:
-            f.write(self.template)
+            f.write(fill_template)
 
     def run_task(self, task_path) -> None:
-        print("run_linux")
+        print(f"Задача {task_path.__str__()} запущена")
+        work_path = os.getcwd()
+        os.chdir(task_path)
+        os.system("./start.sh")
+        os.chdir(work_path)
 
     def make_passport(self, task_path, autotest_config, passport) -> None:
-        with open(passport, "r", encoding='utf-8') as f:
+        with open(passport, "r") as f:
             passport = json.load(f)
-        passport["user"] = os.system("echo @USER")
+        passport["user"] = os.system("echo $USER")
         passport["work_name"] = autotest_config["work_name"]
-        print("passport_linux")
+        with open(task_path / "passport.json", "w") as f:
+            json.dump(passport, f, ensure_ascii=True)
 
 
 def run_task(abstract_class: AbstractTaskRun, template, task_path, autotest_config, passport_template=None):
     abstract_class.template_method(template, task_path, autotest_config, passport_template)
-
-
-if __name__ == "__main__":
-    template = Path(r'D:\project\GIT\poltsh_ref\Lazurit\runTask\templates\run.bat')
-    task_path = Path(r"C:\Users\vvbuley\Desktop\polish_test\calc\lazurit\Lazurit.WIN.AVX.exe\ROG_15_y_0_1")
-    autotest_config = read_file(path=Path(r"C:\Users\vvbuley\Desktop\polish_test"), name_file="start_conf")
-    match platform:
-        case "win32":
-            run_task(WindowsRunTask(), template, task_path, autotest_config)
-        case "linux":
-            run_task(LinuxRunTask(), template, task_path, autotest_config)
-        case _:
-            raise ValueError("Данная система не поддерживает запуск приложения")
